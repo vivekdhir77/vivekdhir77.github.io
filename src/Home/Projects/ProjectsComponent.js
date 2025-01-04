3cb74275abf045739da7106316d002eaa378aa7c
vivekdhir77 <vivekdhir77@gmail.com>
2025-01-03 23:29:16 -0500

    adding gh-pages configs

--- src/Home/Projects/ProjectsComponent.js
+++ src/Home/Projects/ProjectsComponent.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import './ProjectsComponent.css';
 
-const ProjectsComponent = ({ projectName, projectDescription, projectLink, media }) => {
-  return (
-    <div className="project-card">
-      <div className="project-info">
-        <h3>{projectName}</h3>
-        <p>{projectDescription}</p>
-        <a href={projectLink} target="_blank" rel="noopener noreferrer">
-          <button className="view-project-button">View Project</button>
-        </a>
-      </div>
-      <div className="project-media">
-        {media}
-      </div>
-    </div>
-  );
+const ProjectsComponent = ({ title, description, link, image }) => {
+    return (
+        <div className="project-card">
+            <img src={image} alt={title} />
+            <h2>{title}</h2>
+            <p>{description}</p>
+            <a href={link} target="_blank" rel="noopener noreferrer">View Project</a>
+        </div>
+    );
 };
 
 export default ProjectsComponent;
