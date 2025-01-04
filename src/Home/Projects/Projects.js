94183bc0f2e8a344bd398f9c572b1a5a9d94cd32
vivekdhir77 <vivekdhir77@gmail.com>
2025-01-03 23:36:46 -0500

    redeploy

--- src/Home/Projects/Projects.js
+++ src/Home/Projects/Projects.js
@@ -1,28 +1,58 @@
 import React from 'react';
-import ProjectsComponent from './ProjectsComponent';
 import './Projects.css';
+import ProjectsComponent from './ProjectsComponent';
 
 const Projects = () => {
-    return (
-        <div className="projects-container">
-            <h1>Projects</h1>
-            <div className="projects-grid">
-                <ProjectsComponent
-                    title="Project Title 1"
-                    description="Brief description of Project 1."
-                    link="https://link-to-project1.com"
-                    image="path/to/image1.png"
-                />
-                <ProjectsComponent
-                    title="Project Title 2"
-                    description="Brief description of Project 2."
-                    link="https://link-to-project2.com"
-                    image="path/to/image2.png"
-                />
-                {/* Add more projects as needed */}
+  return (
+    <div className="projects-container">
+      <hr />
+      <div className="projects-grid">
+        <ProjectsComponent
+          projectName="One AI Click"
+          projectDescription="LLM fine-tuning abstraction tool"
+          projectLink="https://oneaiclick.com"
+          media={
+            <div className="video-container">
+              <iframe
+                src="https://www.youtube.com/embed/zHRR3NSMXcU"
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
             </div>
-        </div>
-    );
+          }
+        />
+        <hr />
+        <ProjectsComponent
+          projectName="Prime Plot"
+          projectDescription="Empowering Business Owners with Data-Driven Location Choices (HackUmass 2024) (https://devpost.com/software/prime-plot)"
+          projectLink="https://prime-plot.vercel.app/"
+          media={
+            <img
+              src={`${process.env.PUBLIC_URL}/PrimePlot.png`}
+              alt="Project Media"
+              width="480"
+              height="300"
+            />
+          }
+        />
+        <hr />
+        <ProjectsComponent
+          projectName="AI Car"
+          projectDescription="Self driving car simulation using pygame and neural network from ground up."
+          projectLink="https://github.com/vivekdhir77/AI-Car"
+          media={
+            <img
+              src={`${process.env.PUBLIC_URL}/AICar.png`}
+              alt="Project Media"
+              width="480"
+              height="300"
+            />
+          }
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
